762ee893b095a88a96f7eed00d2c39c952010544
Leo Ascenzi <leo.ascenzi@gmail.com>
2025-12-07 14:57:36 -0500

    remove local scripts/workflow

--- build/all_up.sh
+++ build/all_up.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-docker compose -f "../deployments/docker-compose.local.yml" build broker
-docker compose -f "../deployments/docker-compose.local.yml" build generatedata
+docker compose -f "../deployments/docker-compose.yml" build broker
+docker compose -f "../deployments/docker-compose.yml" build generatedata
 echo "Starting Kafka broker container and then waiting 3 seconds"
-docker compose -f "../deployments/docker-compose.local.yml" up -d
+docker compose -f "../deployments/docker-compose.yml" up -d
 sleep 2
 echo "Kafka started, creating trade topics"
 docker exec -it broker /opt/kafka/bin/kafka-topics.sh --create --topic trade-bids --bootstrap-server broker:29092
