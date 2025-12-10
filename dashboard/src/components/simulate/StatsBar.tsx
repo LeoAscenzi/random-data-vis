6e5c6991c56d162f359507987bbd32d9eb8056c0
Leo Ascenzi <leo.ascenzi@gmail.com>
2025-12-09 22:49:26 -0500

    mobile centered stats

--- dashboard/src/components/simulate/StatsBar.tsx
+++ dashboard/src/components/simulate/StatsBar.tsx
@@ -7,14 +7,14 @@ type StatsBarProps = {
 const StatsBar = ({theoreticalRate, producerRate, consumerRate} : StatsBarProps) => 
 {
     return(
-        <div className="grid col-span-8 md:grid-cols-4 grid-cols-8 md:max-w-[45vw] lg:max-w-[40vw] pb-1">
+        <div className="grid col-span-8 md:grid-cols-4 grid-cols-16 md:max-w-[45vw] lg:max-w-[40vw] pb-1">
             <div className="grid md:col-span-1 col-span-2">Stats</div>
             {/* Theoretical */}
-            <div className="grid md:col-span-1 col-span-2">{theoreticalRate} msg/s</div>
+            <div className="grid md:col-span-1 col-span-4 text-center">{theoreticalRate} msg/s</div>
             {/* Producer */}
-            <div className="grid md:col-span-1 col-span-2">{producerRate} msg/s</div>
+            <div className="grid md:col-span-1 col-span-5 text-center">{producerRate} msg/s</div>
             {/* Consumer */}
-            <div className="grid md:col-span-1 col-span-2">{consumerRate} msg/s</div>
+            <div className="grid md:col-span-1 col-span-5 text-center">{consumerRate} msg/s</div>
         </div>
     )
 }
