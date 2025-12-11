4c159100bb523a8f86b7b8af1695ff3b1245d1a4
Leo Ascenzi <leo.ascenzi@gmail.com>
2025-12-11 00:26:58 -0500

    mirror apis

--- backend/java/consumer/src/main/java/com/leoascenzi/consumer/controller/AppController.java
+++ backend/java/consumer/src/main/java/com/leoascenzi/consumer/controller/AppController.java
@@ -1,5 +1,7 @@
 package com.leoascenzi.consumer.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,9 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class AppController {
 
     @GetMapping("/")
-    public String healthcheck()
+    public ResponseEntity<String> healthcheck()
     {
-        return "Server is up!";
+        return new ResponseEntity<String>("{\"status\": \"App is up!\"}", HttpStatus.OK);
     }
     
 }
